e238a39acd71d740e63c6712e3686143f0c83e57
Steve McKay <shubalubdub@gmail.com>
2013-08-06 18:12:52 -0400

    lambdas not a great idea

--- src/peer_connection.cpp
+++ src/peer_connection.cpp
@@ -1,22 +1,51 @@
+#include <functional>
+
 #include "peer_connection.h"
+
+using namespace std::placeholders;
 
 peer_connection::pointer peer_connection::create(boost::asio::io_service &io)
 {
 	return pointer(new peer_connection(io));
 }
 
-tcp::socket& peer_connection::socket()
+boost::asio::ip::tcp::socket& peer_connection::socket()
 {
 	return _socket;
 }
 
 void peer_connection::start()
 {
+	read_length();
 
 }
 
 void peer_connection::handle_write(const boost::system::error_code&, size_t)
 {
 
+}
+
+void peer_connection::read_length()
+{
+	boost::asio::async_read(_socket,
+		boost::asio::buffer(&_receive_len, sizeof(_receive_len)),
+		std::bind(&peer_connection::read_message, this, _1, _2));
+}
+
+void peer_connection::read_message(const boost::system::error_code& error, std::size_t /*bytes_transferred*/)
+{
+	if (!error) {
+		boost::asio::async_read(_socket,
+			boost::asio::buffer(_receive_buf, sizeof(_receive_buf)),
+			std::bind(&peer_connection::handle_message, this, _1, _2));
+	} else {
+		// log and cleanup
+	}
+}
+
+void peer_connection::handle_message(const boost::system::error_code& /* error */, std::size_t /*bytes_transferred*/)
+{
+	// do something
+	read_length();
 }
 
--- src/peer_connection.h
+++ src/peer_connection.h
@@ -1,8 +1,8 @@
-#include <boost/asio.hpp>
-#include <boost/shared_ptr.hpp>
-#include <boost/enable_shared_from_this.hpp>
+#pragma once
 
-using boost::asio::ip::tcp;
+#include <boost/asio.hpp>
+#include <boost/cstdint.hpp>
+#include <boost/enable_shared_from_this.hpp>
 
 class peer_connection
 	: public boost::enable_shared_from_this<peer_connection>
@@ -11,13 +11,19 @@ public:
 	typedef boost::shared_ptr<peer_connection> pointer;
 
 	static pointer create(boost::asio::io_service &io);
-	tcp::socket& socket();
+	boost::asio::ip::tcp::socket& socket();
 	void start();
 private:
 	peer_connection(boost::asio::io_service &io)
 		: _socket(io) { }
-	void handle_write(const boost::system::error_code &error,
-			size_t bytes_written);
-	tcp::socket _socket;
+	void handle_write(const boost::system::error_code &error, size_t bytes_written);
+
+	void read_length();
+	void read_message(const boost::system::error_code &error, size_t bytes_written);
+	void handle_message(const boost::system::error_code &error, size_t bytes_written);
+
+	boost::asio::ip::tcp::socket _socket;
+	boost::uint32_t _receive_len;
+	boost::uint8_t _receive_buf[4096];
 };
 
